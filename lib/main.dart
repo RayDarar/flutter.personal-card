d4aab7d2242511fb17197aa86377d1cfd717b5e5
RayDarar <dfqgth400@gmail.com>
2020-05-27 18:54:22 +0600

    fonts-installing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  runApp(App());
+}
+
+Container genContainer(int index) {
+  return Container(
+    color: Colors.white,
+    height: 100,
+    child: Center(
+        child: Text(
+      index.toString(),
+      style: TextStyle(fontSize: 22),
+    )),
   );
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+              child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/avatar.jpeg"),
+              ),
+              Text(
+                "Ryspekov Ansar",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ))),
+    );
+  }
 }
